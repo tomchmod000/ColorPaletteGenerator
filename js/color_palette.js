beb57c24e375a67448a482eb7af44eed9dda0a11
Tommy <tommych123@gmail.com>
2022-03-07 14:56:45 -0500

    working hex input -> hsl conversion -> able to load into array

--- js/color_palette.js
+++ js/color_palette.js
@@ -110,6 +110,11 @@ https://www.w3schools.com/colors/colors_picker.asp
 
 // Input color / default color (randomly selected when loading page)
 
+let input_hex = "#66ffcc";
+
+let input_hue = 0;
+let input_sat = 0;
+let input_light = 0;
 
 // Color conversions from HSL to HSLA, RGB, RGBA, and Hex
 
@@ -122,6 +127,57 @@ https://www.w3schools.com/colors/colors_picker.asp
 	// HSL to Hex
 
 	// Hex to HSL
+
+	function hexToHSL(H) {
+  // Convert hex to RGB first
+  let r = 0, g = 0, b = 0;
+  if (H.length == 4) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  } else if (H.length == 7) {
+    r = "0x" + H[1] + H[2];
+    g = "0x" + H[3] + H[4];
+    b = "0x" + H[5] + H[6];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r,g,b),
+      cmax = Math.max(r,g,b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+
+  if (delta == 0)
+    h = 0;
+  else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+  else if (cmax == g)
+    h = (b - r) / delta + 2;
+  else
+    h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0)
+    h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+  
+  // set conversion into input
+  input_hue = h;
+  input_sat = s;
+  input_light = l;
+  
+
+  return "hsl(" + h + "," + s + "%," + l + "%)";
+}
 
 
 // Color calculation functions
@@ -181,18 +237,19 @@ https://www.w3schools.com/colors/colors_picker.asp
 		// Test
 		function loadColor() {
 			
-
-			for (let i = 0; i < 7; i++) {
 				let a = 0;
 				let b = 0;
 				let c = 0;
 				let d = 0;
 
-				a = Math.floor(Math.random() * 361);
+				//a = Math.floor(Math.random() * 361);
 				//a = event.target.value;
+				a = input_hue;
 				b = Math.floor(Math.random() * 361);
 				c = Math.floor(Math.random() * 361);
 				d = Math.floor(Math.random() * 361);
+
+			for (let i = 0; i < 7; i++) {
 				  // use array to store a, b, c , d etc 
 					// for actual function assign variablearray[0]  = inputarray[huevalue0];
 					// etc etc
